d7d036e93b66b2a5efa24330bb1ffe7438eb87c1
Femboy <theophile.fouillet@protonmail.com>
2023-03-18 00:41:39 +0100

    Propre

--- launch.sh
+++ launch.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-while true
-do
-    python3 src/main.py
-    echo "Script killed"
-    sleep $(($(date -d "21:03" +%s) - $(date +%s)))
-done
+# Set your bot API key here
+BOT_API_KEY="6089060960:AAEqhHfUVLgfnS0QsbEA4pcRl_jQ1STDQJM"
+CHANNEL_ID="-1001907693181"
+SCRIPT_PATH="src/main.py"
+
+# Start the Python script
+python3 $SCRIPT_PATH
+
+# If the script exits, send a message to the channel indicating the bot crashed
+curl -s -X POST "https://api.telegram.org/bot$BOT_API_KEY/sendMessage" \
+  -d "chat_id=$CHANNEL_ID" \
+  -d "text=The bot has crashed. Please check the logs and restart it."
+
