daf46a9692d4aad625d881362142fa6633681a73
Femboy <theophile.fouillet@protonmail.com>
2023-03-18 21:53:29 +0100

    ss

--- launch.sh
+++ launch.sh
@@ -10,18 +10,16 @@ restart_script() {
     sleep 60
     exec "$0"
 }
-trap restart_script SIGINT SIGTERM
-# Start the Python script
-python3 $SCRIPT_PATH
-restart_script() {
-    echo "Script terminated. Restarting in 1 minute..."
-    sleep 60
-    exec "$0"
-}
+
+# Function to exit the script gracefully
 exit_script() {
     echo "Exiting script gracefully..."
     exit 0
 }
+
+# Trap to handle the script shutdown (SIGINT) and restart (SIGTERM)
+trap exit_script SIGINT
+trap restart_script SIGTERM
 
 sendMessage(){
   # If the script exits, send a message to the channel indicating the bot crashed
@@ -30,10 +28,6 @@ curl -s -X POST "https://api.telegram.org/bot$BOT_API_KEY/sendMessage" \
   -d "text=The bot has crashed. Please check the logs and restart it."
 
 }
-
-# Trap to handle the script shutdown (SIGINT) and restart (SIGTERM)
-trap exit_script SIGINT
-trap restart_script SIGTERM
 
 # Infinite loop
 while true; do
